6b5dd1d998b248ca7f66befc1d545872d8f3cbbf
janawr <jana.wr.9@gmail.com>
2019-11-15 20:14:49 +0300

    Add tests

---
+++ tests/test_hello_world.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from tmp_great_app.tmp_hello_world import hello_world
+
+
+@patch('builtins.print')
+class TestStringMethods(TestCase):
+    def test_hello_world__called_once(self, print_mock):
+        hello_world()
+        print_mock.assert_called_once()
+
+    def test_hello_world__called_with(self, print_mock):
+        hello_world()
+        print_mock.assert_called_with('Hello World!')
+
+    def test_hello_world__called_once_with(self, print_mock):
+        hello_world()
+        print_mock.assert_called_once_with('Hello World!')
